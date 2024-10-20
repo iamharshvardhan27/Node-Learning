d110f719118cb569daafb047107a1c67e7d14955
Harshvardhan Mohite <27harshvardhan@gmail.com>
2024-10-20 11:02:00 +0530

    add 2NodeModule

---
+++ 2NodeModule/addition.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    return a + b;
+}//using simple function syntax
+
+const addthree = (a,b,c) => {
+    return a + b + c;
+}//using arrow function syntax
+
+module.exports = addthree; //used the module.exports to export the module
---
+++ 2NodeModule/calculator.js
@@ -0,0 +1,3 @@
+const addfun = require("./addition"); //used the require to get the module 
+
+console.log(addfun(10,20,30));
